636a3e6e4c9bed3896364f50876484f513f5524b
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-05-19 22:15:38 +0100

    implements create country failure and test api geoname connection

--- tests/test_location/conftest.py
+++ tests/test_location/conftest.py
@@ -1,15 +1,22 @@
+import os
 import pytest
+
+from dotenv import load_dotenv
 from sqlalchemy.orm import Session
 from unittest.mock import Mock
 
 from domain.entities.location import Country
+from domain.exceptions.integrity_error import IntegrityError
 from domain.interfaces.location_repository_interface import LocationRepositoryInterface
-from domain.schemas.location_schema import CountryCreateSchema
+from interface_adapters.database.sqlalchemy.location_repository import SQLAlchemyLocationRepository
 from use_cases.location_use_cases import LocationUseCases
 
 
+load_dotenv()
+
+
 @pytest.fixture
-def mcok_session():
+def mock_session():
     return Mock(spec=Session)
 
 
@@ -24,6 +31,16 @@ def mock_location_use_cases():
 
 
 @pytest.fixture
+def api_geonames_connection_string_success():
+    return os.getenv("API_COUNTRIES")
+
+
+@pytest.fixture
+def api_geonames_connection_string_failure():
+    return "http://api.geonames.org/countryInfoJSON?username=testfailure"
+
+
+@pytest.fixture
 def mock_location_repo_success(mock_location_repo_interface, countries):
     location_repo = mock_location_repo_interface
     location_repo.create_country.return_value = countries
@@ -32,8 +49,9 @@ def mock_location_repo_success(mock_location_repo_interface, countries):
 
 
 @pytest.fixture
-def mock_location_repo_failure(mock_location_repo_interface):
-    location_repo = mock_location_repo_interface
+def mock_location_repo_failure(mock_session):
+    location_repo = SQLAlchemyLocationRepository(session=mock_session)
+    location_repo.create_country = Mock(side_effect=IntegrityError("!!!ERROR"))
 
     return location_repo
 
--- tests/test_location/test_location.py
+++ tests/test_location/test_location.py
@@ -1,9 +1,11 @@
-import pytest
-
 from unittest.mock import Mock
+import pytest
+import requests
+
 from fastapi.encoders import jsonable_encoder
 
 from domain.entities.location import Country
+from domain.exceptions.integrity_error import IntegrityError
 
 
 class TestLocation:
@@ -35,3 +37,20 @@ class TestLocation:
         assert isinstance(new_country[0], Country)
         assert isinstance(new_country[1], Country)
         assert isinstance(new_country[2], Country)
+
+    @pytest.mark.asyncio
+    async def test_create_country_failure(self, mock_location_repo_failure):
+        with pytest.raises(IntegrityError, match="!!!ERROR"):
+            mock_location_repo_failure.create_country()
+
+    @pytest.mark.asyncio
+    async def test_api_geonames_connection_success(self, api_geonames_connection_string_success):
+        api_connection = requests.get(api_geonames_connection_string_success)
+        
+        assert api_connection.status_code == 200
+
+    @pytest.mark.asyncio
+    async def test_api_geonames_connection_failure(self, api_geonames_connection_string_failure):
+        api_connection = requests.get(api_geonames_connection_string_failure)
+
+        assert api_connection.status_code == 401
