04d6031a7951ddf59768473d5d00f964cbfe64b7
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-05-24 13:43:09 +0100

    implements create state tests and rename route tests

--- tests/test_location/test_location_routes.py
+++ tests/test_location/test_location_routes.py
@@ -34,7 +34,7 @@ class TestLocationRoutes:
         assert response.content.decode("utf-8") == "Not Found"
 
     @pytest.mark.asyncio
-    async def test_get_countries_success(self, mock_location_use_cases, countries):
+    async def test_get_countries_route_success(self, mock_location_use_cases, countries):
         mock_location_use_cases.get_countries = Mock(return_value=countries)
 
         response = client.get("/location/country")
@@ -47,14 +47,14 @@ class TestLocationRoutes:
         ]
 
     @pytest.mark.asyncio
-    async def test_get_countries_failure(self):
+    async def test_get_countries_route_failure(self):
         response = client.get("location/countr")
 
         assert response.status_code == 404
         assert response.content.decode("utf-8") == "Not Found"
 
     @pytest.mark.asyncio
-    async def test_get_country_by_id_success(self, mock_location_use_cases, country, country_id=0):
+    async def test_get_country_by_id_route_success(self, mock_location_use_cases, country, country_id=0):
         mock_location_use_cases.get_country_by_id = Mock(return_value=country)
 
         response = client.get(f"/location/country/{country_id}")
@@ -63,14 +63,14 @@ class TestLocationRoutes:
         assert response.json() == {'id': 0, 'common_name': 'Andorra', 'code': 'AD'}
 
     @pytest.mark.asyncio
-    async def test_get_country_by_id_failure(self, country_id=300):
+    async def test_get_country_by_id_route_failure(self, country_id=300):
         response = client.get(f"/location/countr/{country_id}")
 
         assert response.status_code == 404
         assert response.content.decode("utf-8") == "Not Found"
 
     @pytest.mark.asyncio
-    async def test_get_states_success(self, mock_location_use_cases, states):
+    async def test_get_states_route_success(self, mock_location_use_cases, states):
         mock_location_use_cases.get_states = Mock(return_value=states)
 
         response = client.get("/location/country/state/Andorra")
@@ -83,14 +83,14 @@ class TestLocationRoutes:
         ]
 
     @pytest.mark.asyncio
-    async def test_get_states_failure(self):
+    async def test_get_states_route_failure(self):
         response = client.get("/location/stat/Andorra")
 
         assert response.status_code == 404
         assert response.content.decode() == "Not Found"
 
     @pytest.mark.asyncio
-    async def test_get_state_by_id_success(self, mock_location_use_cases, state, state_id=0):
+    async def test_get_state_by_id_route_success(self, mock_location_use_cases, state, state_id=0):
         mock_location_use_cases.get_state_by_id = Mock(return_value=state)
 
         response = client.get(f"/location/state/{state_id}")
@@ -100,8 +100,28 @@ class TestLocationRoutes:
         assert response.json()["id"] == state_id
 
     @pytest.mark.asyncio
-    async def test_get_state_by_id_failure(self):
+    async def test_get_state_by_id_route_failure(self):
         response = client.get("/location/stat/99")
+
+        assert response.status_code == 404
+        assert response.content.decode() == "Not Found"
+
+    @pytest.mark.asyncio
+    async def test_create_state_route_success(self, mock_location_use_cases, create_state_data, state):
+        mock_location_use_cases.create_state = Mock(return_value=state)
+
+        state_data = create_state_data.model_dump()
+
+        response = client.post("/location/state/", json=state_data)
+
+        assert response.status_code == 201
+        assert response.json() == {'id': 0, 'name': 'Escaldes-Engordany', 'code': '08', 'admin_code': 8, 'country_id': 1}
+
+    @pytest.mark.asyncio
+    async def test_create_state_route_failure(self, create_state_data):
+        state_data = create_state_data.model_dump()
+
+        response = client.post("/location/stat/", json=state_data)
 
         assert response.status_code == 404
         assert response.content.decode() == "Not Found"
