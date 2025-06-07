50b5f0835e0a4d0315816c2abf30f3f667616fc2
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-06-07 10:21:18 +0100

    creates interview and interview type table

---
+++ domain/entities/interview.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+from domain.entities.base import Base
+
+
+class Interview(Base):
+    __tablename__ = "interviews"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    interview_type = Column(ForeignKey("interview_types.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+    result = Column(String, nullable=False)
+    interview_date = Column(DateTime, nullable=False)
+    job_id = Column(Integer, ForeignKey("jobs.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
+
+    job = relationship("Job", back_populates="interview")
+    interview_type = relationship("InterviewType", back_populates="interview")
+
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=True)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
---
+++ domain/entities/interview_type.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+
+from domain.entities.base import Base
+
+
+class InterviewType(Base):
+    __tablename__ = "interview_types"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    interview_type = Column(String, nullable=False, unique=True)
+
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=True)
+
+    job = relationship("Interview", back_populates="interview_type", cascade="all, delete-orphan")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
